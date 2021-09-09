1b1dac75543172597188cfcff1628ff8089012d4
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-09-09 12:18:01 -0600

    Vista

---
+++ CarlosJavierJimenezVasquez/CarlosJavierJimenezVasquez/Controllers/CapturarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarlosJavierJimenezVasquez.Controllers
+{
+    public class CapturarController : Controller
+    {
+        // GET: Capturaar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult Monto()
+        {
+            return View();
+        }
+    }
+}
